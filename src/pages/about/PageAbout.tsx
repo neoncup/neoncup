e8b520aa2268db277d93316124d51db0d966f115
Meireles Fernando <neoncup.me@gmail.com>
2024-07-08 20:02:19 +0100

    clean all files to start a new project.

--- src/pages/about/PageAbout.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import { About } from './styled'
-
-
-export const PageAbout: React.FC = () => {
-    return (
-        <About><h1>About Me... :)</h1></About>
-    )
-}
--- src/pages/pages.ts
+++
@@ -1,8 +0,0 @@
-// ALL Pages here
-
-
-export * from './home/PageHome'
-export * from './projects/PageProjects'
-export * from './work/PageWork'
-export * from './contacts/PageContacts'
-export * from './about/PageAbout'
